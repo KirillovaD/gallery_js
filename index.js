21d5b4b599121de98f834dd1f185bde3231eca42
KirillovaD <sdaria@inbox.ru>
2022-07-11 12:42:54 +0300

    feat: fix prev button

--- index.js
+++ index.js
@@ -9,34 +9,31 @@ let imageSrc = ['assets/images/image1.jpg','assets/images/image2.jpg','assets/im
 
 //5. Путь к картинки равен инекдсу в массиве, а индекс в массиве соотв счетчику наших картинок
 let imageIndex = 0;
-
 image.src = imageSrc[imageIndex];
-
-
-
-
 //2. Объявляем к кнопкам функциям
-function nextImage(){
-  //прибавляем на 1 при  нажатии на кнопку
-  imageIndex ++;
-  
-  // присваиваем новое значение индексу в массиве с путями
-  image.src = imageSrc[imageIndex];
-  
+ //прибавляем на 1 при  нажатии на кнопку
+ // присваиваем новое значение индексу в массиве с путями
   // чтобы остановился на количестве картинок кот есть и начать снова
+function nextImage(){
   if (imageIndex === (imageSrc.length - 1)){
     imageIndex = 0;
+    image.src = imageSrc[imageIndex];
   }
+  else {
+    imageIndex ++;
+    image.src = imageSrc[imageIndex];
+  }
+  
 }
-
+// уменьшаем на 1 при  нажатии на кнопку
 function prevImage(){
-  //уменьшаем на 1 при  нажатии на кнопку
-  imageIndex --;
-  image.src = imageSrc[imageIndex];
   if (imageIndex === 0){
-    imageIndex === (imageSrc.length);
+    imageIndex = (imageSrc.length - 1);
+    image.src = imageSrc[imageIndex];
   }
-
+  else {
+    imageIndex --;
+    image.src = imageSrc[imageIndex];
 }
-
+}
 
